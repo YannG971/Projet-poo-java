5722c910c80e68b6bdf54855eea9be5d9d8aab05
YannG971 <149069713+YannG971@users.noreply.github.com>
2024-04-21 21:59:43 +0200

    Add files via upload

---
+++ DoctolibECE.java
@@ -0,0 +1,8 @@
+import GUI.ChoixComptes;
+
+public class DoctolibECE {
+    public static void main(String[] args) {
+        ChoixComptes choixComptes = new ChoixComptes();
+        choixComptes.setVisible(true);
+    }
+}
